84f2971aa2fcbe42cf44d87797750ed2dd48bb2d
AndrewCski <138247589+AndrewCski@users.noreply.github.com>
2025-08-14 14:22:16 -0700

    Add files via upload

---
+++ first_modelling_attempt/attempt 22 (Dust Modelling)/Case 2/Ganymede_Dust_Modelling_Test.m
@@ -0,0 +1,93 @@
+% constants
+
+omega = 2*pi/(11.23*3600);                            % synodic frequency (rad/s)
+R_G = 2631e3;                                         % Ganymede radius (m)
+c = 299792458;                                        % speed of light (m/s)
+
+% simulation parameters
+
+elements = "dust";
+
+[init_r, init_v_vectors, N_total, particle_traits] = Io_Dust_Starting_States(...
+    [10,10], [-1.25*R_G,1.25*R_G], [-1.25*R_G,1.25*R_G], -3*R_G, 200000, 5000, 10e-9, 2500);
+
+% timestep = 2 * pi * m / (100 * q * 3.85 * 10^-7);     % 500 steps / gyration, 3.85.. ave B field
+timestep = 0.005;
+start = 0 * (2 * pi / omega)/4;                       % point in synodic period to start from
+
+% numsteps = round(abs((2 * pi / omega) / timestep)); % steps for a synodic period
+numsteps = 20000;                                     % doesnt tend to need above this number
+sim_size = 4 * R_G;
+
+B0_vec = [-15 * 10 ^-9; 24 * 10 ^-9; 0]; % seems to oscillate, idk about the amplitude rn... 
+
+case_id = "Ganymede";
+% case_id = "yeah";
+
+time = linspace(start, start + numsteps * timestep, numsteps);
+
+r_cell = cell(N_total, 1);           % position (m)
+v_cell = cell(N_total, 1);           % velocity (m/s)
+
+num_recorded = ones(N_total, 1);     % number of timesteps recorded for each particles
+escaped = ones(N_total, 1);          % whether particles trace back to Europa or not
+impact_coords = zeros(N_total, 6);   % coords before and after going through surface
+
+%%
+
+for p_index = 1:N_total
+    % initialize per-particle arrays (overallocate to max possible)
+    r_temp = zeros(3, numsteps);
+    v_temp = zeros(3, numsteps);
+    
+    % initial state
+    r_now = init_r(p_index, :)';
+    v_now = init_v_vectors(p_index, :)';
+    
+    r_temp(:,1) = r_now;
+    v_temp(:,1) = v_now;
+
+    for u = 1:numsteps-1
+        B_prim_now = real(B0_vec .* exp(1i * omega * time(u))); % B_z set in Boris loop
+        
+        [r_new, v_new] = new_relativistic_Boris(r_now, v_now, timestep, B_prim_now, ...
+            0, case_id, particle_traits(p_index, 2), particle_traits(p_index, 1));
+        
+        r_now = r_new;
+        v_now = v_new;
+
+        r_temp(:, u+1) = r_now;
+        v_temp(:, u+1) = v_now;
+        
+        num_recorded(p_index) = u + 1;
+
+        r_norm2 = sum(r_now.^2);  % avoid sqrt for speed
+        if r_norm2 <= R_G^2
+            escaped(p_index) = 0;
+            impact_coords(p_index, 1:3) = r_temp(:, u);
+            impact_coords(p_index, 4:6) = r_temp(:, u+1);
+            break;
+        % elseif r_norm2 > (sim_size)^2
+        elseif r_norm2 > (sim_size)^2 || r_now(1) > 1.5 * R_G % particles are probably not coming back...
+            break;
+        end
+    end
+    
+    % save truncated histories
+    r_cell{p_index} = r_temp(:, 1:num_recorded(p_index));
+    v_cell{p_index} = v_temp(:, 1:num_recorded(p_index));
+    
+    if mod(p_index, 500) == 0
+        fprintf('%d/%d\n', p_index, N_total);
+    end
+end
+
+%%
+
+Particle_Trajectories(r_cell, "idk", elements, 0, "Ganymede");
+forward_particle_start_accessibilities(init_r, escaped, 0, particle_traits(p_index, 1)./ ...
+    particle_traits(p_index, 2), "Ganymede")
+% forward_particle_start_accessibilities(init_r, escaped, [2,2], particle_traits(p_index, 1)./ ...
+%    particle_traits(p_index, 2), "Ganymede")
+forward_particle_impact_accessibilities(impact_coords, 5, particle_traits(p_index, 1)./ ...
+    particle_traits(p_index, 2), "Ganymede")
